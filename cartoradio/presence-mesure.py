451b290296695e41eab3cd3d88724057d8b0382f
crep7424 <29643360+crep7424@users.noreply.github.com>
2022-10-27 21:58:33 +0200

    extraction de la pageavec la mesure

--- cartoradio/presence-mesure.py
+++ cartoradio/presence-mesure.py
@@ -1,6 +1,6 @@
 from PyPDF2 import PdfFileReader
 
-fichier = PdfFileReader('RPUB_150497.pdf')
+fichier = PdfFileReader('cartoradio/RPUB_150497.pdf')
 signets = fichier.outline
 
 def check_is_mesure_exist(o):
@@ -9,12 +9,37 @@ def check_is_mesure_exist(o):
             is_exist = check_is_mesure_exist(i)
         else:
             if i.title == "Résultat de la mesure spécifique*":
-                return i.page
+                
+                return i
         if 'is_exist' in locals() and is_exist != None:
             return is_exist
                 
         
 
-
-if check_is_mesure_exist(signets) != None:
+is_mesure_exist=check_is_mesure_exist(signets)
+if is_mesure_exist != None:
     print("WWWWWwwOOOOOOOOOOO")
+
+    print(type(is_mesure_exist))
+    npage=fichier.get_destination_page_number(is_mesure_exist)
+    print(type(npage))
+    print(npage)
+    page=fichier.getPage(npage)
+
+    print(type(page.get_contents()))
+    print(page.get_contents())
+
+    txt=page.extractText()
+    # txt.replace('\x13 e', 'é')
+    # txt.replace('\x13E', 'É')
+    # txt.replace('\x12 a', 'à')
+    # txt.replace('\x12 e', 'è')
+    # txt.replace('\'', "'")
+    # txt.replace('\\', '"')
+    # txt.replace('\\x0', 'f')
+    print(type(txt))
+    print(txt)
+
+    
+
+    print("wowowo")
