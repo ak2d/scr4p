d08480cdf7c9692a464d3b7cef68b84804c250bd
crep7424 <crep7424@gmail.com>
2022-12-01 10:02:00 +0100

    [cartoradio] extraction des valeurs avec regex + mise en fonction

--- cartoradio/presence-mesure.py
+++ cartoradio/presence-mesure.py
@@ -1,7 +1,7 @@
 from PyPDF2 import PdfFileReader
+import re
 
-fichier = PdfFileReader('cartoradio/RPUB_150497.pdf')
-signets = fichier.outline
+f = PdfFileReader('cartoradio/RPUB_150497.pdf')
 
 def check_is_mesure_exist(o):
     for i in o:
@@ -15,31 +15,46 @@ def check_is_mesure_exist(o):
             return is_exist
                 
         
+def get_mesure(fichier):
+    signets = fichier.outline
 
-is_mesure_exist=check_is_mesure_exist(signets)
-if is_mesure_exist != None:
-    print("WWWWWwwOOOOOOOOOOO")
+    is_mesure_exist=check_is_mesure_exist(signets)
+    if is_mesure_exist != None:
+        #print("WWWWWwwOOOOOOOOOOO")
 
-    print(type(is_mesure_exist))
-    npage=fichier.get_destination_page_number(is_mesure_exist)
-    print(type(npage))
-    print(npage)
-    page=fichier.getPage(npage)
+        #print(type(is_mesure_exist))
+        npage=fichier.get_destination_page_number(is_mesure_exist)
+        #print(type(npage))
+        #print(npage)
+        page=fichier.getPage(npage)
 
-    print(type(page.get_contents()))
-    print(page.get_contents())
+        #print(type(page.get_contents()))
+        #print(page.get_contents())
 
-    txt=page.extractText()
-    # txt.replace('\x13 e', 'é')
-    # txt.replace('\x13E', 'É')
-    # txt.replace('\x12 a', 'à')
-    # txt.replace('\x12 e', 'è')
-    # txt.replace('\'', "'")
-    # txt.replace('\\', '"')
-    # txt.replace('\\x0', 'f')
-    print(type(txt))
-    print(txt)
+        txt=page.extractText()
+        # txt.replace('\x13 e', 'é')
+        # txt.replace('\x13E', 'É')
+        # txt.replace('\x12 a', 'à')
+        # txt.replace('\x12 e', 'è')
+        # txt.replace('\'', "'")
+        # txt.replace('\\', '"')
+        # txt.replace('\\x0', 'f')
+        #print(type(txt))
+        #print(txt)
 
-    
+        
 
-    print("wowowo")
+        #print("wowowowowowowowowowowowo")
+        rawvalues = re.findall(r'[0-9]*\.?[0-9]* V\/m', txt)
+        #print(type(rawvalues))
+        #print(rawvalues)
+
+        values=[]
+        for i in rawvalues:
+            values.append(float(i[:-3]))
+        #print(type(values))
+        #print(values)
+        return values
+        
+ab = get_mesure(f)
+print(ab)
